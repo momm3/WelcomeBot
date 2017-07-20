642312e69590a2637c51ab5b64f584275306210d
Marvin Scott <momm3iskool@gmail.com>
2017-07-20 10:17:30 +0000

    First Commit

---
+++ dict.py
@@ -0,0 +1,77 @@
+{
+    "ok": true,
+    "members": [
+        {
+            "id": "U023BECGF",
+            "team_id": "T021F9ZE2",
+            "name": "bobby",
+            "deleted": false,
+            "color": "9f69e7",
+            "real_name": "Bobby Tables",
+            "tz": "America/Los_Angeles",
+            "tz_label": "Pacific Daylight Time",
+            "tz_offset": -25200,
+            "profile": {
+                "avatar_hash": "ge3b51ca72de",
+                "current_status": ":mountain_railway: riding a train",
+                "first_name": "Bobby",
+                "last_name": "Tables",
+                "real_name": "Bobby Tables",
+                "email": "bobby@slack.com",
+                "skype": "my-skype-name",
+                "phone": "+1 (123) 456 7890",
+                "image_24": "https://...",
+                "image_32": "https://...",
+                "image_48": "https://...",
+                "image_72": "https://...",
+                "image_192": "https://..."
+            },
+            "is_admin": true,
+            "is_owner": true,
+            "updated": 1490054400,
+            "has_2fa": false
+        },
+        {
+            "id": "W07QCRPA4",
+            "team_id": "T0G9PQBBK",
+            "name": "glinda",
+            "deleted": false,
+            "color": "9f69e7",
+            "real_name": "Glinda Southgood",
+            "tz": "America/Los_Angeles",
+            "tz_label": "Pacific Daylight Time",
+            "tz_offset": -25200,
+            "profile": {
+                "avatar_hash": "8fbdd10b41c6",
+                "image_24": "https://a.slack-edge.com...png",
+                "image_32": "https://a.slack-edge.com...png",
+                "image_48": "https://a.slack-edge.com...png",
+                "image_72": "https://a.slack-edge.com...png",
+                "image_192": "https://a.slack-edge.com...png",
+                "image_512": "https://a.slack-edge.com...png",
+                "image_1024": "https://a.slack-edge.com...png",
+                "image_original": "https://a.slack-edge.com...png",
+                "first_name": "Glinda",
+                "last_name": "Southgood",
+                "title": "Glinda the Good",
+                "phone": "",
+                "skype": "",
+                "real_name": "Glinda Southgood",
+                "real_name_normalized": "Glinda Southgood",
+                "email": "glenda@south.oz.coven"
+            },
+            "is_admin": true,
+            "is_owner": false,
+            "is_primary_owner": false,
+            "is_restricted": false,
+            "is_ultra_restricted": false,
+            "is_bot": false,
+            "updated": 1480527098,
+            "has_2fa": false
+        }
+    ],
+    "cache_ts": 1498777272,
+    "response_metadata": {
+        "next_cursor": "dXNlcjpVMEc5V0ZYTlo="
+    }
+}
